6d27ad608c0bb61bf7d920a612b7145b1223a9d4
KostiaSA <savchenkov@buhta.ru>
2016-09-25 18:19:30 +0300

    смена названия на Бухта WMS

---
+++ android/app/src/main/java/com/buhtawms/MainApplication.java
@@ -0,0 +1,44 @@
+package com.buhtawms;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.wenkesj.voice.VoicePackage;
+import com.eguma.barcodescanner.BarcodeScannerPackage;
+import com.mihir.react.tts.*;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    protected boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+          new VectorIconsPackage(),
+          new VoicePackage(),
+          new BarcodeScannerPackage(),
+          new RCTTextToSpeechModule()
+      );
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+      return mReactNativeHost;
+  }
+}
