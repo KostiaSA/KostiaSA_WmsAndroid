5c7cdf87d0449d6c816a50edc99594a714ef71be
KostiaSA <sonyk795>
2016-10-05 17:23:29 +0300

    -

--- src/config/BuhtaServer.ts
+++ src/config/BuhtaServer.ts
@@ -1,8 +1,8 @@
 
 
 export function getBuhtaServerAddress(): string {
-    //return "192.168.0.14:3000";
-    return "5.19.239.191:3000";
+    //return "192.168.0.14:3001";
+    return "5.19.239.191:3001";
 }
 export function getBuhtaServerKey(): string {
     return "QLj0dJ2DfgnsOyLhlc0z+Z7ThSxVajqX";
